9e11b76c607c9564da7e4dd735b9dec367bd0213
ParkerChen <jchen440@my.bcit.ca>
2025-02-06 22:29:46 -0800

    Update gas_station.py

--- gas_station.py
+++ gas_station.py
@@ -1,8 +1,8 @@
 class Solution:
     def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
         """
-        Problem: Given an array gas where gas[i] is the amount of gas at the ith station, 
-        and an array cost where cost[i] is the cost to travel from the ith station to the next one, 
+        Problem: Given an array gas where gas[i] is the amount of gas at the station, 
+        and an array cost where cost[i] is the cost to travel from the station to the next one, 
         return the starting gas station's index if you can travel around the circuit once in the clockwise direction, 
         otherwise return -1. If there exists a solution, it is guaranteed to be unique.
         
